74246f5247959d862ab4dc61649517c14625fc6f
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-07-03 16:44:36 -0400

    Refactor spec support files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,5 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'eatabit_rails'
 require 'vcr'
-
-VCR.configure do |c|
-  c.cassette_library_dir = "spec/fixtures"
-  c.hook_into :webmock
-end
+require 'support/vcr'
+require 'support/configuration'
