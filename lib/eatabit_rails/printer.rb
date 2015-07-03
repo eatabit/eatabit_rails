df071209fd5519cf2944d81e6a3735e3f3099050
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-07-03 16:45:05 -0400

    EatabitRails::Printer.find should return a EatabitRails::Printer

--- lib/eatabit_rails/printer.rb
+++ lib/eatabit_rails/printer.rb
@@ -4,12 +4,25 @@ module EatabitRails
 
     PATH = 'printers'
 
+    attr_reader :printer
+
+    def initialize(attributes)
+      @name             = attributes['name']
+      @enabled          = attributes['enabled']
+      @pickup_minutes   = attributes['pickup_minutes']
+      @delivery_minutes = attributes['delivery_minutes']
+      @state            = attributes['state']
+      @online           = attributes['online']
+      @has_paper        = attributes['has_paper']
+    end
+
     def self.find(id)
       printer_uri = EatabitRails::REST::Uri.new.printer id
       params      = EatabitRails::REST::Uri.default_params
       response    = RestClient.get printer_uri, params
+      attributes  = JSON.parse(response.body)['printer']
 
-      JSON.parse response.body
+      new(attributes)
     end
   end
 end
