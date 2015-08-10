359a6431339d6f696627d10a6cf4e799c0f9c185
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-08-10 16:58:47 -0400

    Add v4 Account support

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe EatabitRails::Account do
 
     describe '.find' do
 
-      let(:account) { find_account }
+      let(:account) { find_account('v2') }
 
       it 'should return a EatabitRails::Account' do
         expect(account).to be_a(EatabitRails::Account)
@@ -26,11 +26,30 @@ RSpec.describe EatabitRails::Account do
         expect(account.created_at).to be
       end
     end
+  end
 
-    def find_account
-      VCR.use_cassette('account') do
-        EatabitRails::Account.find
+  context 'v4', version: :v4 do
+
+    describe '.find' do
+
+      let(:account) { find_account('v4') }
+
+      it 'should return a EatabitRails::Account' do
+        expect(account).to be_a(EatabitRails::Account)
       end
+
+      it 'should assign attributes' do
+        expect(account.name).to eq('eatabit.io Test')
+        expect(account.environment).to eq('development')
+        expect(account.enabled).to be
+        expect(account.created_at).to be
+      end
+    end
+  end
+
+  def find_account(version)
+    VCR.use_cassette("account_#{version}") do
+      EatabitRails::Account.find
     end
   end
 end
